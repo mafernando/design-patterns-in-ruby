d10ca214b25e2a86e217db2ee0fbca9afc929dc2
Michael Fernando <michael@Michaels-MacBook-Pro.local>
2015-10-07 01:09:52 -0700

    Added truth, lies and nil section

--- sandbox/sandbox.rb
+++ sandbox/sandbox.rb
@@ -1,3 +1,32 @@
+# Truth, Lies, and Nil
+# DPiR_page: 28
+
+p 1 == 1 # true
+p 1 == 2 # false
+p 'russ' == 'smart' # false
+p (1 < 2) # true
+p (4 > 6) # true
+
+a = 1
+b = 10000
+p a > b # false
+
+p 4 >= 4 # true
+p 1 <= 2 # true
+
+p (1 == 1) and (2 == 2) # true
+p (1 == 1) and (2 == 3) # false
+
+p (1 == 1) && (2 == 2) # true
+p (1 == 1) && (2 == 3) # false
+
+p (not(1 == 2))  # true
+p !(1 == 1)     # true
+p (not(false))     # true
+
+p (true and 'fred') # fred (Note: while considered "true" will print as 'fred' - last value returned)
+p ('fred' && 44)    # 44 (Note: while considered "true" will print as 44 - last value returned)
+p (nil || false)    # false
 
 # A simple example demonstrating if and elsif statements
 # DPiR_page: 31
