b6b1ff2ebbf47578660eff490fbf5e12491823dd
mafernando <fernando_michael@bah.com>
2015-10-08 14:57:07 -0500

    Add procs and blocks section
    A proc and a block are similar.
    Procs are like object handles on a block.

--- notes/examples.rb
+++ notes/examples.rb
@@ -504,3 +504,104 @@ module Banking
 end
 
 x = Banking::Portfolio.new :michael
+
+# Procs and Blocks
+# DPiR: 84
+
+# proc example - lambda same as Proc.new
+# 1) code block, lambda, and closure are roughly similar
+# 2) lambda's can access the context that it was created in
+# 3) a block is just part of the *syntax* of a method call. It
+# can only appear in argument lists. there can be only one!
+proc_var = 'Michael'
+hello = lambda do
+  p "Hello #{proc_var}"
+  p 'I am inside a proc'
+end
+
+# how to execute a proc
+hello.call
+
+# equivalent proc declaration to above
+hello = lambda {
+  p 'Hello, I am inside a proc'
+}
+
+# pure block which will error since only
+# lambdas can be declared as an object
+# that can be instantiated
+# hello = {
+#   p 'Hello, I am inside a proc'
+# }
+
+# procs also returned last value touched
+return_24 = lambda {24}
+p return_24.call
+
+# procs can accept params too
+multiply = lambda {|x, y| x * y}
+n = multiply.call(20, 3)
+p n
+n = multiply.call(10, 50)
+p n
+
+# we can pass a proc to run in a method
+# by appending it to the end of method call. the
+# method will transfer control to the block at yield
+# and then resume once the block is evaluated
+def run_it
+  p 'Before the yield'
+  #yield
+  p 'After the yield'
+end
+
+# Should Output:
+#   Before the yield
+#   Hello
+#   Coming to you from inside the block
+#   After the yield
+run_it do
+  p 'Hello'
+  p 'Coming to you from inside the block'
+end
+
+# example of passing control to proc block
+# by appending block to end of method.
+# here the yield is a proxy for proc.call
+def yield_with_parameter
+  p 'Before the yield with param'
+  yield(24)
+  p 'After the yield with param'
+end
+yield_with_parameter do |x|
+  p 'Hello from inside the proc'
+  p "The value of x is #{x}"
+end
+
+# example of passing control to proc
+# block as a parameter
+# use the ampersand syntax (&) to make explicit that a proc is being passed.
+# ( add & when param is defined and passed )
+# can't pass params included with a lambda passed as an arg, need
+# send each part separately and assemble correctly prior to proc_block call.
+# that's why we have num1 and num2 being passed
+def run_it_with_parameter(num1, num2, &proc_block) # param defined (&)
+  p 'Before the call to proc block'
+  p "Output of #{proc_block.to_s}: " + proc_block.call(num1, num2).to_s
+  p 'After the call to proc block'
+end
+my_proc_block = lambda { |x| p "The value of x is #{x} in the proc" }
+run_it_with_parameter(1, 2, &multiply)  # param passed (&)
+
+# http://www.skorks.com/2013/04/ruby-ampersand-parameter-demystified/
+# example of sending multiple proc.
+# can only seem to pass a single lambda with ampersand for some reason, idk why
+lambda1 = lambda { |x| p "hello #{x} buddy" }
+lambda2 = lambda { |x| p "hello #{x} pal" }
+lambda3 = lambda { |x| p "hello #{x} friend" }
+def multi_lambda_params(p1, p2, &p3)
+  p1.call(1111)
+  p2.call(2222)
+  p3.call(3333)
+end
+multi_lambda_params(lambda1, lambda2, &lambda3)
